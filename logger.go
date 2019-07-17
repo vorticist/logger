1d80890bb6b2dc4f95a6a88bf40d6fb76443035c
Aaron Vega <me@vorticist.net>
2019-07-17 11:17:50 -0700

    Initial commit

---
+++ logger.go
@@ -0,0 +1,76 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+}
+
+func Trace(args ...interface{}) {
+	log.Trace(args...)
+}
+
+func Tracef(format string, args ...interface{}) {
+	log.Tracef(format, args...)
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
+}
+
+func Info(args ...interface{}) {
+	log.Info(args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+func Print(args ...interface{}) {
+	log.Print(args...)
+}
+
+func Printf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+func Warn(args ...interface{}) {
+	log.Warn(args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	log.Warnf(format, args...)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+
+func Fatal(args ...interface{}) {
+	log.Fatal(args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	log.Fatalf(format, args...)
+}
+
+func Panic(args ...interface{}) {
+	log.Panic(args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	log.Panicf(format, args...)
+}
